4bdc79d3bc2cd86c9668d61d73042d5452ff573d
waltjohnson <walter.hyrum@gmail.com>
2020-10-31 02:33:52 -0600

    Working halloween project

--- halloween/README.md
+++ halloween/README.md
@@ -1,9 +1,8 @@
 Halloween Sound Effects
 
-# Setup
+# Setup 
 
 1. Setup cron job to run for the user on startup.  Don't do this as sudo as it will attempt to run as root and the audio will not work.
-
 ```
 $ crontab -e
 
@@ -11,12 +10,17 @@ $ crontab -e
 ```
 
 2. On Raspberry Pi, set default audio to 3.5mm audio jack.
-
 ```
 $ raspi-config
 
 Force audio to 3.5 jack
 ```
 
+3. On Raspberry Pi, uncomment and set `hdmi_force_hotplug=1` in `/boot/config.txt` to pretend HDMI hotplug signal is asserted so it appears HDMI display is attached.  Uses HDMI mode even though no monitor is attached.  This is necessary to enable audio, otherwise we get no sound.
+```
+$ sudo vi /boot/config
+
+hdmi_force_hotplug=1
+```
 
 
--- halloween/run.py
+++ halloween/run.py
@@ -2,6 +2,14 @@ from sense_hat import SenseHat
 from time import sleep
 import vlc
 # import os
+import RPi.GPIO as GPIO
+
+# PIN DEFINITIONS
+# GND - pin 39
+pin_beam = 26       # pin 37
+pin_doorbell = 19   # pin 35
+pin_other1 = 13     # pin 33
+pin_other2 = 6      # pin 31
 
 def play_file(filename):
     print("Playing: " + filename)
@@ -9,9 +17,37 @@ def play_file(filename):
     p.play()
     # os.system("play " + filename)
 
+def handle_event(pin):
+    if pin == pin_beam:
+        play_file("/home/pi/projects/halloween/Effects/owl.wav")
+    if pin == pin_doorbell:
+        play_file("/home/pi/projects/halloween/Effects/evillaff4.wav")
+    if pin == pin_other1:
+        play_file("/home/pi/projects/halloween/Effects/cackle07.wav")
+    if pin == pin_other2:
+        play_file("/home/pi/projects/halloween/Effects/Raven_Sound_Clip.mp3")
+
+def check_gpio_toggle(pin, level_last):
+    level = GPIO.input(pin)
+    # print("Level: %d %d" % (level, level_last[0]))
+    if level==0 and level_last[0]==1:
+        print("GPIO %d event" % (pin))
+        handle_event(pin)
+    level_last[0] = level
+
+###############################################
 # Main
 sense = SenseHat()
 # sense.show_message("Boo!!!", scroll_speed=0.05)
+
+# GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin_doorbell, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(pin_beam, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(pin_other1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(pin_other2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# GPIO.setup(channel, GPIO.OUT)
 
 X = [255, 0, 0]  # Red
 # O = [255, 255, 255]  # White
@@ -42,27 +78,37 @@ O, X, X, X, X, X, X, O
 sense.set_pixels(isaac)
 
 run = True
+level_beam = [0]
+level_doorbell = [0]
+level_other1 = [0]
+level_other2 = [0]
 
 while run:
-    sleep(0.1)
-    event = sense.stick.wait_for_event()
-    print("The joystick was {} {}".format(event.action, event.direction))
+    sleep(0.2)
 
-    if event.action == "pressed":
-        if event.direction == "middle":
-            run = False
+    check_gpio_toggle(pin_beam, level_beam)
+    check_gpio_toggle(pin_doorbell, level_doorbell)
+    check_gpio_toggle(pin_other1, level_other1)    
+    check_gpio_toggle(pin_other2, level_other2)
 
-        if event.direction == "up":
-            play_file("/home/pi/projects/halloween/Effects/owl.wav")
+    for event in sense.stick.get_events():
+    # event = sense.stick.wait_for_event()
+        print("The joystick was {} {}".format(event.action, event.direction))
 
-        if event.direction == "down":
-            play_file("/home/pi/projects/halloween/Effects/evillaff4.wav")
+        if (event.action == "pressed"):
+            if event.direction == "middle":
+                run = False
 
-        if event.direction == "left":
-            play_file("/home/pi/projects/halloween/Effects/cackle07.wav")
-
-        if event.direction == "right":
-            play_file("/home/pi/projects/halloween/Effects/Raven_Sound_Clip.mp3")
+            if event.direction == "up":
+                handle_event(pin_beam)
+            if event.direction == "right":
+                handle_event(pin_doorbell)
+            if event.direction == "down":
+                handle_event(pin_other1)
+            if event.direction == "left":
+                handle_event(pin_other2)
 
 print("Quiting")
 sense.clear()
+# Free up resources and return I/O to inputs
+GPIO.cleanup()
