3d5caa6259f02f1b9fb1044e09d1d9a01603bcb4
waltjohnson <walter.hyrum@gmail.com>
2020-10-31 17:43:48 -0600

    Added redeyes

--- halloween/run.py
+++ halloween/run.py
@@ -1,5 +1,5 @@
 from sense_hat import SenseHat
-from time import sleep
+import time
 import vlc
 # import os
 import RPi.GPIO as GPIO
@@ -8,10 +8,13 @@ class halloween:
     def __init__(self):
         # PIN DEFINITIONS
         # GND - pin 39
-        self.pin_beam = 26       # pin 37
-        self.pin_doorbell = 19   # pin 35
-        self.pin_other1 = 13     # pin 33
-        self.pin_other2 = 6      # pin 31
+        self.pin_beam = 26          # pin 37
+        self.pin_doorbell = 19      # pin 35
+        self.pin_dooropen = 13      # pin 33
+        self.pin_other = 6          # pin 31
+        self.pin_redeyes = 5        # pin 29
+        self.redeyes_starttime = 0.0
+        self.redeyes_state = 0
 
     def play_file(self, filename):
         print("Playing: " + filename)
@@ -23,13 +26,16 @@ class halloween:
         if pin == self.pin_beam:
             self.sense.set_pixel(7,0,0,255,0)
             self.play_file("/home/pi/projects/halloween/Effects/owl.wav")
+            self.redeyes_starttime = time.time()
         if pin == self.pin_doorbell:
             self.sense.set_pixel(7,1,0,255,0)
             self.play_file("/home/pi/projects/halloween/Effects/evillaff4.wav")
-        if pin == self.pin_other1:
-            self.sense.set_pixel(7,2,0,255,0)
-            self.play_file("/home/pi/projects/halloween/Effects/cackle07.wav")
-        if pin == self.pin_other2:
+        if pin == self.pin_dooropen:
+            self.sense.set_pixel(7,2,0,255,0)            
+            self.play_file("/home/pi/projects/halloween/Effects/large_metal_rusty_door.mp3")
+            self.play_file("/home/pi/projects/halloween/Effects/old_door_creaking.mp3") 
+            # self.play_file("/home/pi/projects/halloween/Effects/cackle07.wav")
+        if pin == self.pin_other:
             self.sense.set_pixel(7,3,0,255,0)
             self.play_file("/home/pi/projects/halloween/Effects/Raven_Sound_Clip.mp3")
 
@@ -41,6 +47,21 @@ class halloween:
             self.handle_event(pin)
         level_last[0] = level
 
+    def blink_red_eyes(self):
+        dt = time.time() - self.redeyes_starttime
+        # print("dt: %d" % dt)
+        if dt > 5.0 and dt < 20.0:
+            if dt < 10.0:
+                if self.redeyes_state:
+                    self.redeyes_state = 0
+                else:
+                    self.redeyes_state = 1
+                GPIO.output(self.pin_redeyes, self.redeyes_state)    # Toggle
+            else:
+                GPIO.output(self.pin_redeyes, 1)    # On
+        else:
+            GPIO.output(self.pin_redeyes, 0)    # Off
+
     def main(self):
         self.sense = SenseHat()
         # sense.show_message("Boo!!!", scroll_speed=0.05)
@@ -49,10 +70,11 @@ class halloween:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.pin_doorbell, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.pin_beam, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(self.pin_other1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(self.pin_other2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.pin_dooropen, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.pin_other, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-        # GPIO.setup(channel, GPIO.OUT)
+        GPIO.setup(self.pin_redeyes, GPIO.OUT)
+        GPIO.output(self.pin_redeyes, 0)    # Off
 
         X = [255, 0, 0]  # Red
         # O = [255, 255, 255]  # White
@@ -85,11 +107,11 @@ class halloween:
         run = True
         level_beam = [0]
         level_doorbell = [0]
-        level_other1 = [0]
-        level_other2 = [0]
+        level_dooropen = [0]
+        level_other = [0]
 
         while run:
-            sleep(0.25)
+            time.sleep(0.2)
 
             # Clear 
             for i in range(4):
@@ -97,8 +119,10 @@ class halloween:
 
             self.check_gpio_toggle(self.pin_beam, level_beam)
             self.check_gpio_toggle(self.pin_doorbell, level_doorbell)
-            self.check_gpio_toggle(self.pin_other1, level_other1)    
-            self.check_gpio_toggle(self.pin_other2, level_other2)
+            self.check_gpio_toggle(self.pin_dooropen, level_dooropen)    
+            self.check_gpio_toggle(self.pin_other, level_other)
+
+            self.blink_red_eyes()
 
             for event in self.sense.stick.get_events():
             # event = self.sense.stick.wait_for_event() # blocks
@@ -113,9 +137,9 @@ class halloween:
                     if event.direction == "right":
                         self.handle_event(self.pin_doorbell)
                     if event.direction == "down":
-                        self.handle_event(self.pin_other1)
+                        self.handle_event(self.pin_dooropen)
                     if event.direction == "left":
-                        self.handle_event(self.pin_other2)
+                        self.handle_event(self.pin_other)
 
         print("Quiting")
         self.sense.clear()
